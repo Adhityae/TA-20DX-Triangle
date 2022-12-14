61610a41f4982e515bfe96991397a91e48049600
AdeliaRiskhiArishandi <adelia89ayishandi@gmail.com>
2022-12-15 00:25:13 +0700

    Adelia: Edit signin file

---
+++ application/views/signin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width,initial-scale=1, maximum-scale=1,shrink-to-fit=no" name="viewport">
+    <title>Login | Our Teacher</title>
+    <style>
+        
+        *{
+            font-family: 'Inter', sans-serif;
+        }
+        body{
+            background: linear-gradient(180deg, #FFFFFF 0%, #EEFEFF 100%);
+            height: 100%;
+        }
+        .section{
+            height: 100%;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
